6d940261a95123418ce64c91f3fcb3394e237441
aswin <aswintimalsina4@.com>
2026-06-26 12:34:45 +0545

    fixed sitemap namespace trailing slash

--- app.js
+++ app.js
@@ -574,7 +574,7 @@ app.get('/sitemap.xml', async (req, res) => {
   const posts = USE_MONGO
     ? await Post.find({ status: { $ne: 'draft' } })
     : readDB().posts.filter(p => p.status !== 'draft');
-  let xml = `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9/">
+  let xml = `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url><loc>${base}/</loc><priority>1.0</priority></url>
   <url><loc>${base}/blog</loc><priority>0.9</priority></url>
   <url><loc>${base}/gallery</loc><priority>0.8</priority></url>
