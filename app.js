e75f0d911ba965bc589a6dddc4987443c9bc42d9
aswin <aswintimalsina4@.com>
2026-09-14 12:40:27 +0545

    fix CSP blocking inline event handlers

--- app.js
+++ app.js
@@ -160,6 +160,8 @@ app.use(helmet({
     directives: {
       defaultSrc: ["'self'"],
       scriptSrc: ["'self'", "'unsafe-inline'"],
+      scriptSrc: ["'self'", "'unsafe-inline'"],
+      scriptSrcAttr: ["'unsafe-inline'"],
       styleSrc: ["'self'", "'unsafe-inline'"],
       imgSrc: ["'self'", "data:", "https:"],
       fontSrc: ["'self'", "data:"],
